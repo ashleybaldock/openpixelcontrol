2f55ec686ee1bb315270c917ec04ccbfc2513868
Ashley Baldock <ashley.baldock@bbc.co.uk>
2019-01-30 18:07:42 +0000

    Working dome code

--- genDome.js
+++ genDome.js
@@ -1,6 +1,6 @@
 const X = 0.525731112119133606;
 const Z = 0.850650808352039932;
-const viso = [
+const verticesIsocahedron = [
             [-X, 0.0, Z], [ X, 0.0, Z ], [ -X, 0.0, -Z ], [ X, 0.0, -Z ],
             [ 0.0, Z, X ], [ 0.0, Z, -X ], [ 0.0, -Z, X ], [ 0.0, -Z, -X ],
             [ Z, X, 0.0 ], [ -Z, X, 0.0 ], [ Z, -X, 0.0 ], [ -Z, -X, 0.0 ]
@@ -47,6 +47,31 @@ const vNorm = (v) => {
   return vDiv(v, vMag(v));
 };
 
+const vRotX = (v, t) => {
+  // |1     0           0| |x|   |        x        |   |x'|
+  // |0   cos θ    −sin θ| |y| = |y cos θ − z sin θ| = |y'|
+  // |0   sin θ     cos θ| |z|   |y sin θ + z cos θ|   |z'|
+  return [v[0], v[1] * Math.cos(t) - v[2] * Math.sin(t), v[1] * Math.sin(t) + v[2] * Math.cos(t)];
+};
+
+const vRotY = (v, t) => {
+  // | cos θ    0   sin θ| |x|   | x cos θ + z sin θ|   |x'|
+  // |   0      1       0| |y| = |         y        | = |y'|
+  // |−sin θ    0   cos θ| |z|   |−x sin θ + z cos θ|   |z'|
+  return [v[0] * Math.cos(t) + v[2] * Math.sin(t), v[1], v[2] * Math.cos(t) - v[0] * Math.sin(t) ];
+};
+
+const vRotZ = (v, t) => {
+  // |cos θ   −sin θ   0| |x|   |x cos θ − y sin θ|   |x'|
+  // |sin θ    cos θ   0| |y| = |x sin θ + y cos θ| = |y'|
+  // |  0       0      1| |z|   |        z        |   |z'|
+  return [v[0] * Math.cos(t) - v[1] * Math.sin(t), v[0] * Math.sin(t) + v[1] * Math.cos(t), v[2]];
+};
+
+
+const angle = Math.asin(X);
+const rotatedVertices = verticesIsocahedron.map(v => vRotY(v, angle));
+
 const edges = [];
 const pushUnique = (a, b) => {
   if (!edges.some(([ea, eb]) => vEqual(ea, a) && vEqual(eb, b) || vEqual(ea, b) && vEqual(eb, a))) {
@@ -71,23 +96,25 @@ const subdivide = (a, b, c) => {
 };
 
 indices.forEach(([a, b, c]) => {
-  subdivide(viso[a], viso[b], viso[c]);
-  //pushUnique(viso[a], viso[b]);
-  //pushUnique(viso[b], viso[c]);
-  //pushUnique(viso[a], viso[c]);
-});
-
-const filteredEdges = edges.filter(([a, b]) => {
-  return true;//a[1] > 0 || b[1] > 0;
+  subdivide(rotatedVertices[a], rotatedVertices[b], rotatedVertices[c]);
+  //subdivide(verticesIsocahedron[a], verticesIsocahedron[b], verticesIsocahedron[c]);
+  //pushUnique(rotatedVertices[a], rotatedVertices[b]); pushUnique(rotatedVertices[b], rotatedVertices[c]); pushUnique(rotatedVertices[a], rotatedVertices[c]);
+  //pushUnique(verticesIsocahedron[a], verticesIsocahedron[b]); pushUnique(verticesIsocahedron[b], verticesIsocahedron[c]); pushUnique(verticesIsocahedron[a], verticesIsocahedron[c]);
 });
 
 const nLEDs = 32;
-const rawdata = [];
-filteredEdges.forEach(([from, to]) => {
-  for (let i = 1; i < nLEDs + 1; i++) {
-    rawdata.push(vMul(vLerp(from, to, 1 / (nLEDs + 1) * i), 2));
-  }
-});
+const lerp = edges => {
+  let out = [];
+  edges.forEach(([from, to]) => {
+    for (let i = 1; i < nLEDs + 1; i++) {
+      out.push(vMul(vLerp(from, to, 1 / (nLEDs + 1) * i), 2));
+    }
+  });
+  return out;
+};
+
+//const cutDome = edges => edges.filter(([a, b]) => a[2] >= -0.1 && b[2] >= -0.1);
+const cutDome = edges => edges.filter(([a, b]) => a[2] >= 0.1 || b[2] >= 0.1);
 
 const unique = a => {
   return a.filter(function(item, index, allItems) {
@@ -95,9 +122,14 @@ const unique = a => {
   })
 };
 
-//const vdata = unique(rawdata.sort());
-const vdata = rawdata;//.sort();
-console.error(indices.length, edges.length, filteredEdges.length, vdata.length, rawdata.length);
+const domeEdges = cutDome(edges);
+const lerped = lerp(domeEdges);
+const vUnique = unique(lerped)
+const vdata = vUnique.sort();
+
+//const vdata = cutDome(unique(rawdata.sort()));
+//const vdata = rawdata;//.sort();
+console.error(indices.length, lerped.length, vUnique.length, domeEdges.length, vdata.length);
 
 console.log(`[`);
 for (let i = 0; i < vdata.length - 1; i++) {
