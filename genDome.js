e05f4421e028d0b3fbf6c0859e6185ec9a9d25ea
Ashley Baldock <ashley@baldock.me>
2019-02-15 00:04:35 +0000

    Configuring specific channel paths on simulated dome

--- genDome.js
+++ genDome.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const X = 0.525731112119133606;
 const Z = 0.850650808352039932;
 const verticesIsocahedron = [
@@ -102,8 +104,83 @@ indices.forEach(([a, b, c]) => {
   //pushUnique(verticesIsocahedron[a], verticesIsocahedron[b]); pushUnique(verticesIsocahedron[b], verticesIsocahedron[c]); pushUnique(verticesIsocahedron[a], verticesIsocahedron[c]);
 });
 
-const nLEDs = 32;
-const lerp = edges => {
+//const cutDome = edges => edges.filter(([a, b]) => a[2] >= -0.1 && b[2] >= -0.1);
+const cutDome = edges => edges.filter(([a, b]) => a[2] >= 0.1 || b[2] >= 0.1);
+
+const domeEdges = cutDome(edges);
+//console.error(domeEdges);
+
+
+const graph = (() => {
+  const nodes = [];
+  const links = [];
+
+  const makeNode = ([x, y, z]) => ({ x, y, z });//, name: `${x},${y},${z}` });
+  const makeEdge = (source, target) => ({ source, target });
+
+  const nodesEqual = (node1, node2) => node1.x === node2.x && node1.y === node2.y && node1.z === node2.z;
+  const edgesEqual = (edge1, edge2) => edge1.source === edge2.target && edge1.target === edge2.source || edge1.source === edge2.source && edge1.target === edge2.target;
+
+  const addUniqueNode = newNode => {
+    if (!nodes.some(node => nodesEqual(node, newNode))) { nodes.push(newNode); }
+  };
+
+  const addUniqueEdge = (newNode1, newNode2) => {
+    addUniqueNode(newNode1);
+    addUniqueNode(newNode2);
+    const node1Index = nodes.findIndex(node => nodesEqual(newNode1, node));
+    const node2Index = nodes.findIndex(node => nodesEqual(newNode2, node));
+    const newEdge = makeEdge(node1Index, node2Index);
+
+    if (!links.some(edge => edgesEqual(edge, newEdge))) { links.push(newEdge); }
+  };
+
+  return {
+    nodes,
+    links,
+    addEdge: ([node1, node2]) => {
+      // Add nodes if they aren't in nodes array already
+      addUniqueEdge(makeNode(node1), makeNode(node2));
+    }
+  }
+})();
+console.error(graph);
+
+domeEdges.forEach(edge => {
+  graph.addEdge(edge);
+});
+
+const channels = [
+  [4, 10, 7], [4, 7, 1], [4, 1, 3], [4, 3, 5], [4, 11, 3], [7, 9, 8], [1, 6, 7], [1, 2, 3],
+  [23, 24, 8], [23, 8, 7], [23, 6, 8], [23, 20, 6], [23, 19, 20], [23, 25, 19], [6, 0, 1], [20, 21, 0],
+  [15, 16, 22], [15, 22, 21], [15, 21, 2], [15, 18, 19], [15, 14, 18], [18, 17, 19], [21, 18, 20], [2, 0, 20],
+  [5, 12, 3], [5, 2, 22], [5, 22, 13], [5, 13]
+];
+
+const links = [];
+channels.forEach(channel => {
+  links.push({ source: channel[0], target: channel[1] });
+  if (channel[2] !== undefined) { links.push({ source: channel[1], target: channel[2] }); }
+});
+
+graph.links = links;
+
+
+fs.writeFileSync('./graphFile.json', JSON.stringify(graph, null, 2));
+console.error(graph);
+
+
+const channelOrderedEdges = graph.links.map(link => {
+  return [
+    [graph.nodes[link.source].x, graph.nodes[link.source].y, graph.nodes[link.source].z],
+    [graph.nodes[link.target].x, graph.nodes[link.target].y, graph.nodes[link.target].z]
+  ];
+});
+
+
+const unique = a => a.filter((item, index, allItems) => allItems.findIndex(element => vEqual(element, item)) === index);
+
+const lerp = (nLEDs, edges) => {
   let out = [];
   edges.forEach(([from, to]) => {
     for (let i = 1; i < nLEDs + 1; i++) {
@@ -113,23 +190,14 @@ const lerp = edges => {
   return out;
 };
 
-//const cutDome = edges => edges.filter(([a, b]) => a[2] >= -0.1 && b[2] >= -0.1);
-const cutDome = edges => edges.filter(([a, b]) => a[2] >= 0.1 || b[2] >= 0.1);
-
-const unique = a => {
-  return a.filter(function(item, index, allItems) {
-    return allItems.findIndex(element => vEqual(element, item)) === index;
-  })
-};
-
-const domeEdges = cutDome(edges);
-const lerped = lerp(domeEdges);
-const vUnique = unique(lerped)
-const vdata = vUnique.sort();
+const lerped = lerp(32, channelOrderedEdges);
+//const vUnique = unique(lerped)
+//const vdata = vUnique.sort();
+const vdata = lerped;
 
 //const vdata = cutDome(unique(rawdata.sort()));
 //const vdata = rawdata;//.sort();
-console.error(indices.length, lerped.length, vUnique.length, domeEdges.length, vdata.length);
+console.error(indices.length, lerped.length, channelOrderedEdges.length, domeEdges.length, vdata.length);
 
 console.log(`[`);
 for (let i = 0; i < vdata.length - 1; i++) {
